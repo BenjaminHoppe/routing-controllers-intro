eed22bcdcb76d036862ac122dcd3bf336a02a29f
Benjamin <hoppeconnect@gmail.com>
2018-12-04 18:50:35 -0500

    updated class headers for each instance method

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,15 +1,15 @@
 class PagesController < ApplicationController
 
   def welcome
-
+    @header = "Look it's the welcome page"
   end
 
   def about
-
+    @header = "Oh hey, this is about us"
   end
 
   def contest
-
+    @header = "Contests!"
   end
 
 end
